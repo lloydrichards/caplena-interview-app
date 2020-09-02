e4b291a96cb8fb8b72654586d385637ef0cc5a0f
Lloyd Richards <59728961+LloydRichardsIntern@users.noreply.github.com>
2020-09-02 11:24:48 +0200

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import ReviewText from './components/ReviewText';
+import { Grid, ListItem, Typography, List } from '@material-ui/core';
+import ApprovedText from './components/ApprovedText';
+import RejectedText from './components/RejectedText';
 
-function App() {
+export type Quote = {
+  id: string;
+  sentiment: number;
+  text: string;
+  author: string;
+};
+
+const App = () => {
+  const [unreviewed, setUnreviewed] = useState<Array<Quote>>();
+  const [positive, setPositive] = useState<Array<Quote>>();
+  const [negative, setNegative] = useState<Array<Quote>>();
+
+  useEffect(() => {
+    try {
+      fetch('https://type.fit/api/quotes').then(function (response) {
+        return response.json().then(function (data) {
+          data.forEach((i: any) => {
+            i.sentiment = Math.floor(Math.random() * 3);
+            i.id = nanoid();
+          });
+          setUnreviewed(data);
+          console.log(data);
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  if (!unreviewed) return <div />;
+
+  const currentText = unreviewed[0];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Grid container style={{ minHeight: '300px', margin: '6rem 0' }}>
+        <Grid item xs={12} sm={3} />
+        <Grid item xs={12} sm={6}>
+          <ReviewText
+            quote={currentText}
+            handleApprove={() => {
+              console.log('Approved');
+              setPositive(
+                positive ? [currentText, ...positive] : [currentText]
+              );
+              setUnreviewed(unreviewed.filter((i) => i.id !== currentText.id));
+            }}
+            handleReject={() => {
+              console.log('Rejected');
+              setNegative(
+                negative ? [currentText, ...negative] : [currentText]
+              );
+              setUnreviewed(unreviewed.filter((i) => i.id !== currentText.id));
+              console.log(unreviewed);
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={3} />
+      </Grid>
+
+      <Grid container style={{ padding: '2em' }}>
+        <Grid item xs={6}>
+          <Typography component='span' variant='h4' color='textPrimary'>
+            Correct
+          </Typography>
+          <List>
+            {positive?.map((i) => (
+              <ApprovedText quote={i} key={i.id} />
+            ))}
+          </List>
+        </Grid>
+        <Grid item xs={6} style={{ textAlign: 'right' }}>
+          <Typography component='span' variant='h4' color='textPrimary'>
+            Incorrect
+          </Typography>
+          <List>
+            {negative?.map((i) => (
+              <RejectedText quote={i} key={i.id} />
+            ))}
+          </List>
+        </Grid>
+      </Grid>
     </div>
   );
-}
+};
 
 export default App;
